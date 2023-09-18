7afedde8f33a2578b0ba97a518cbc61f06097891
Allison Briñez <allison.mitchelle@gmail.com>
2023-09-17 19:33:45 -0500

    agrego la carpeta quinto trimestre y paso el registro a Angular

---
+++ QuintoTrimestre/proyecto/src/app/components/registro/registro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.scss']
+})
+export class RegistroComponent {
+
+
+  enviarFormulario(){
+    console.log('formulario enviado');
+  }
+}
