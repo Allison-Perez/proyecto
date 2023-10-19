626bca5c76a7f1425e94085324b0b989963a2c88
Allison Briñez <allison.mitchelle@gmail.com>
2023-10-18 20:49:54 -0500

    prueba nuevo

---
+++ sextoTrimestre/proyecto/src/app/components/prueba/prueba.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentepruebaComponent } from './componenteprueba/componenteprueba.component';
+
+
+
+@NgModule({
+  declarations: [
+    ComponentepruebaComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PruebaModule { }
