0874cd122742e87d5f1a379bb40e37e7cec4f48e
Allison Briñez <allison.mitchelle@gmail.com>
2023-11-02 22:38:09 -0500

    nada importante

--- sextoTrimestre/proyecto/src/app/components/allison/allison-routing.module.ts
+++ sextoTrimestre/proyecto/src/app/components/allison/allison-routing.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+  import { NgModule } from '@angular/core';
+  import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
-import { RecuperarComponent } from './recuperar/recuperar.component';
-import { ValidacionComponent } from './validacion/validacion.component';
+  import { LoginComponent } from './login/login.component';
+  import { RegistroComponent } from './registro/registro.component';
+  import { RecuperarComponent } from './recuperar/recuperar.component';
+  import { ValidacionComponent } from './validacion/validacion.component';
+  // import { RoleGuard } from './login/role.guard';
+import { IndexKatalinaComponent } from '../katalina/index.katalina/index.katalina.component';
+import { VistaAdminComponent } from '../sergio/vista-admin/vista-admin.component';
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'registro', component: RegistroComponent },
-  { path: 'recuperar', component: RecuperarComponent },
-  { path: 'validacion/:correo', component: ValidacionComponent },
-];
+  const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
+    { path: 'recuperar', component: RecuperarComponent },
+    { path: 'validacion/:correo', component: ValidacionComponent },
+    // { path: 'index.katalina', component: IndexKatalinaComponent, canActivate: [RoleGuard], data: { expectedRole: 2 } },
+    // { path: 'admin-perfil', component: VistaAdminComponent, canActivate: [RoleGuard], data: { expectedRole: 3 } },
+  ];
 
-@NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
-export class AllisonRoutingModule { }
+  @NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+  })
+  export class AllisonRoutingModule { }
