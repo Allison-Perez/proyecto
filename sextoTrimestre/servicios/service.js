9c21fc112567db7eb6157880e5b358a1a411bd48
Marlon <mconda7@soy.sena.edu.co>
2023-11-06 22:15:39 -0500

    Funcionando listar todos los componentes y blog completamente

--- sextoTrimestre/servicios/service.js
+++ sextoTrimestre/servicios/service.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcrypt");
+const multer = require('multer');
 const cors = require('cors');
 const crypto = require('crypto');
-
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -17,6 +18,7 @@ const dbConfig = {
   password: "",
   database: "acanner",
 };
+
 
 
 function generarContrasenaTemporal() {
@@ -314,7 +316,386 @@ app.post("/api/cambiar-contrasena", async (req, res) => {
   }
 });
 
+// BLOG
 
+/// Ruta para crear un blog
+app.post('/api/blog/create', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig); // Crear una nueva conexión
+
+    const { titulo, contenido } = req.body;
+    const insertQuery = 'INSERT INTO noticias (titulo, contenido) VALUES (?, ?)';
+
+    await connection.execute(insertQuery, [titulo, contenido]); // Usar la conexión para realizar la consulta
+
+    // Cerrar la conexión y enviar la respuesta
+    connection.end();
+    res.status(200).json({ message: 'Noticia creada exitosamente' });
+  } catch (error) {
+    console.error('Error al insertar la noticia en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo crear la noticia' });
+  }
+});
+
+// Ruta para actualizar una noticia por su ID
+app.put('/api/blog/update/:id_noticias', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig); // Crear una nueva conexión
+
+    const { titulo, contenido } = req.body;
+    const { id_noticias } = req.params;
+    const updateQuery = 'UPDATE noticias SET titulo = ?, contenido = ? WHERE id_noticias = ?';
+
+    await connection.execute(updateQuery, [titulo, contenido, id_noticias]); // Usar la conexión para realizar la consulta
+
+    // Cerrar la conexión y enviar la respuesta
+    connection.end();
+    res.status(200).json({ message: 'Noticia actualizada exitosamente' });
+  } catch (error) {
+    console.error('Error al actualizar la noticia en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo actualizar la noticia' });
+  }
+});
+
+// Ruta para eliminar una noticia por su ID
+app.delete('/api/blog/delete/:id_noticias', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig); 
+
+    const { id_noticias } = req.params;
+    const deleteQuery = 'DELETE FROM noticias WHERE id_noticias = ?';
+
+    await connection.execute(deleteQuery, [id_noticias]); // Usar la conexión para realizar la consulta
+
+    // Cerrar la conexión y enviar la respuesta
+    connection.end();
+    res.status(200).json({ message: 'Noticia eliminada exitosamente', id_noticias });
+  } catch (error) {
+    console.error('Error al eliminar la noticia de la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo eliminar la noticia' });
+  }
+});
+
+// Ruta para obtener la lista de blogs
+app.get('/api/blog/list', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig); 
+    const selectQuery = 'SELECT * FROM noticias';
+
+    const [results] = await connection.execute(selectQuery); 
+
+    connection.end();
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error al obtener la lista de noticias:', error);
+    res.status(500).json({ error: 'No se pudo obtener la lista de noticias' });
+  }
+});
+
+// Subir Archivos
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); 
+  },
+});
+
+app.use('/uploads', express.static('uploads'));
+const upload = multer({ storage });
+
+// ACTIVIDAD
+
+// Ruta para crear una actividad
+app.post('/api/actividad/create', upload.single('archivo'), async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { nombreArchivo, comentario } = req.body;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : '';
+
+    if (!req.file) {
+      console.error('No se ha proporcionado un archivo para subir.');
+      res.status(400).json({ error: 'No se ha proporcionado un archivo para subir' });
+      return;
+    }
+
+    const insertQuery = 'INSERT INTO guias (nombreArchivo, comentario, archivoUrl) VALUES (?, ?, ?)';
+    
+    await connection.execute(insertQuery, [nombreArchivo, comentario, archivoUrl]);
+
+    connection.end();
+    res.status(200).json({ message: 'Actividad creada exitosamente' });
+  } catch (error) {
+    console.error('Error al insertar la actividad en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo crear la actividad' });
+  }
+});
+
+// Ruta para actualizar una actividad por su ID
+app.put('/api/actividad/update/:id_guia', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { nombreArchivo, comentario } = req.body;
+    const { id_actividad } = req.params;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : null;
+
+    let updateQuery;
+    let queryParams;
+
+    if (archivoUrl) {
+      updateQuery = 'UPDATE guias SET nombreArchivo = ?, comentario = ?, archivoUrl = ? WHERE id_guia = ?';
+      queryParams = [nombreArchivo, comentario, archivoUrl, id_actividad];
+    } else {
+      updateQuery = 'UPDATE guias SET nombreArchivo = ?, comentario = ? WHERE id_guia = ?';
+      queryParams = [nombreArchivo, comentario, id_actividad];
+    }
+
+    await connection.execute(updateQuery, queryParams);
+
+    connection.end();
+    res.status(200).json({ message: 'Actividad actualizada exitosamente' });
+  } catch (error) {
+    console.error('Error al actualizar la actividad en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo actualizar la actividad' });
+  }
+});
+
+// Ruta para eliminar una actividad por su ID
+app.delete('/api/actividad/delete/:id_guia', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { id_actividad } = req.params;
+    const deleteQuery = 'DELETE FROM guias WHERE id_guia = ?';
+    
+    await connection.execute(deleteQuery, [id_actividad]);
+
+    connection.end();
+    res.status(200).json({ message: 'Actividad eliminada exitosamente' });
+  } catch (error) {
+    console.error('Error al eliminar la actividad de la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo eliminar la actividad' });
+  }
+});
+
+
+// Ruta para obtener la lista de actividades
+app.get('/api/actividad/list', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const selectQuery = 'SELECT * FROM guias';
+
+    const [results] = await connection.execute(selectQuery);
+
+    connection.end();
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error al obtener la lista de actividades:', error);
+    res.status(500).json({ error: 'No se pudo obtener la lista de actividades' });
+  }
+});
+
+// ASISTENCIA
+
+// Ruta para crear una asistencia
+app.post('/api/asistencia/create', upload.single('archivo'), async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    
+    const { nombreArchivo, comentario } = req.body;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : '';
+
+    if (!req.file) {
+      console.error('No se ha proporcionado un archivo para subir.');
+      res.status(400).json({ error: 'No se ha proporcionado un archivo para subir' });
+      return;
+    }
+
+    const insertQuery = 'INSERT INTO asistencia (nombreArchivo, comentario, archivoUrl) VALUES (?, ?, ?';
+
+    await connection.execute(insertQuery, [nombreArchivo, comentario, archivoUrl]);
+
+    connection.end();
+    res.status(200).json({ message: 'Asistencia creada exitosamente' });
+  } catch (error) {
+    console.error('Error al insertar la asistencia en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo crear la asistencia' });
+  }
+});
+
+// Ruta para actualizar una asistencia por su ID
+app.put('/api/asistencia/update/:id_asistencia', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { nombreArchivo, comentario } = req.body;
+    const { id_asistencia } = req.params;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : null;
+
+    let updateQuery;
+    let queryParams;
+
+    if (archivoUrl) {
+      updateQuery = 'UPDATE asistencia SET nombreArchivo = ?, comentario = ?, archivoUrl = ? WHERE id_asistencia = ?';
+      queryParams = [nombreArchivo, comentario, archivoUrl, id_asistencia];
+    } else {
+      updateQuery = 'UPDATE asistencia SET nombreArchivo = ?, comentario = ? WHERE id_asistencia = ?';
+      queryParams = [nombreArchivo, comentario, id_asistencia];
+    }
+
+    await connection.execute(updateQuery, queryParams);
+
+    connection.end();
+    res.status(200).json({ message: 'Asistencia actualizada exitosamente' });
+  } catch (error) {
+    console.error('Error al actualizar la asistencia en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo actualizar la asistencia' });
+  }
+});
+
+// Ruta para eliminar una asistencia por su ID
+app.delete('/api/asistencia/delete/:id_asistencia', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { id_asistencia } = req.params;
+    const deleteQuery = 'DELETE FROM asistencia WHERE id_asistencia = ?';
+
+    await connection.execute(deleteQuery, [id_asistencia]);
+
+    connection.end();
+    res.status(200).json({ message: 'Asistencia eliminada exitosamente' });
+  } catch (error) {
+    console.error('Error al eliminar la asistencia de la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo eliminar la asistencia' });
+  }
+});
+
+// Ruta para obtener la lista de asistencia
+app.get('/api/asistencia/list', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const selectQuery = 'SELECT * FROM asistencia';
+
+    const [results] = await connection.execute(selectQuery);
+
+    connection.end();
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error al obtener la lista de asistencia:', error);
+    res.status(500).json({ error: 'No se pudo obtener la lista de asistencia' });
+  }
+});
+
+
+// Descargar archivos de asistencia
+app.get('/api/asistencia/download/:archivo', (req, res) => {
+  const archivo = req.params.archivo;
+  const rutaArchivo = path.join(__dirname, 'uploads', archivo);
+  console.log('Ruta del archivo a descargar:', rutaArchivo);
+
+  res.download(rutaArchivo, (err) => {
+    if (err) {
+      console.error('Error al descargar el archivo:', err);
+      res.status(500).end();
+    }
+  });
+});
+
+// HORARIOS
+
+app.post('/api/horario/create',upload.single('archivo'), async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    
+    const { nombreArchivo, comentario } = req.body;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : '';
+
+    if (!req.file) {
+      console.error('No se ha proporcionado un archivo para subir.');
+      res.status(400).json({ error: 'No se ha proporcionado un archivo para subir' });
+      return;
+    }
+
+    const insertQuery = 'INSERT INTO horario (nombreArchivo, comentario, archivoUrl) VALUES (?, ?, ?)';
+
+    await connection.execute(insertQuery, [nombreArchivo, comentario, archivoUrl]);
+
+    connection.end();
+    res.status(200).json({ message: 'Horario creado exitosamente' });
+  } catch (error) {
+    console.error('Error al insertar el horario en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo crear el horario' });
+  }
+});
+
+// Ruta para actualizar un horario por su ID
+app.put('/api/horario/update/:id_horario', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { nombreArchivo, comentario } = req.body;
+    const { id_horario } = req.params;
+    const archivoUrl = req.file ? `/uploads/${req.file.filename}` : null;
+
+    let updateQuery;
+    let queryParams;
+
+    if (archivoUrl) {
+      updateQuery = 'UPDATE horario SET nombreArchivo = ?, comentario = ?, archivoUrl = ? WHERE id_horario = ?';
+      queryParams = [nombreArchivo, comentario, archivoUrl, id_horario];
+    } else {
+      updateQuery = 'UPDATE horario SET nombreArchivo = ?, comentario = ? WHERE id_horario = ?';
+      queryParams = [nombreArchivo, comentario, id_horario];
+    }
+
+    await connection.execute(updateQuery, queryParams);
+
+    connection.end();
+    res.status(200).json({ message: 'Horario actualizado exitosamente' });
+  } catch (error) {
+    console.error('Error al actualizar el horario en la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo actualizar el horario' });
+  }
+});
+
+// Ruta para eliminar un horario por su ID
+app.delete('/api/horario/delete/:id_horario', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+
+    const { id_horario } = req.params;
+    const deleteQuery = 'DELETE FROM horario WHERE id_horario = ?';
+
+    await connection.execute(deleteQuery, [id_horario]);
+
+    connection.end();
+    res.status(200).json({ message: 'Horario eliminado exitosamente' });
+  } catch (error) {
+    console.error('Error al eliminar el horario de la base de datos:', error);
+    res.status(500).json({ error: 'No se pudo eliminar el horario' });
+  }
+});
+
+// Ruta para obtener la lista de horarios
+app.get('/api/horario/list', async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    const selectQuery = 'SELECT * FROM horario';
+
+    const [results] = await connection.execute(selectQuery);
+
+    connection.end();
+    res.status(200).json(results);
+  } catch (error) {
+    console.error('Error al obtener la lista de horarios:', error);
+    res.status(500).json({ error: 'No se pudo obtener la lista de horarios' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`);
