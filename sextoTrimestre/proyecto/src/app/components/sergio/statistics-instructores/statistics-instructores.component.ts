49c035748bdadca356b06f7e6c31c610802f61e3
Allison Briñez <allison.mitchelle@gmail.com>
2024-02-25 16:42:39 -0500

    modifique la barra, y hace filtrado de instructores

--- sextoTrimestre/proyecto/src/app/components/sergio/statistics-instructores/statistics-instructores.component.ts
+++ sextoTrimestre/proyecto/src/app/components/sergio/statistics-instructores/statistics-instructores.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../service/service.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-statistics-instructores',
   templateUrl: './statistics-instructores.component.html',
-  styleUrls: ['./statistics-instructores.component.scss']  // Puedes ajustar esto según tus estilos
+  styleUrls: ['./statistics-instructores.component.scss']
 })
 export class StatisticsInstructoresComponent implements OnInit {
   instructores: any[] = [];
   filteredInstructores: any[] = [];
-  selectednumeroFicha: string = '';
+  originalInstructores: any[] = [];
+  filtroFicha: number | null = null;
+  filtroNombre: string | null = null;
+  filtroDocumento: number | null = null;
+  listaFichas: number[] = [2558104, 1800002, 11231236, 2634256, 2789008];
+  private unsubscribe$ = new Subject<void>();
+
 
   constructor(private service: ServiceService) {}
 
   ngOnInit(): void {
     this.fetchStaticsInstructores();
+
   }
 
   fetchStaticsInstructores(): void {
@@ -22,6 +32,7 @@ export class StatisticsInstructoresComponent implements OnInit {
       (data) => {
         this.instructores = data;
         this.filteredInstructores = data;
+        this.originalInstructores = data.slice();
         console.log('Todos los instructores:', this.instructores);
       },
       (error) => {
@@ -30,18 +41,44 @@ export class StatisticsInstructoresComponent implements OnInit {
     );
   }
 
-  // applyFilters(): void {
-  //   console.log('Ficha seleccionada:', this.selectednumeroFicha);
+  aplicarFiltros() {
+    let usuariosFiltrados = this.originalInstructores.slice();
 
-  //   this.filteredInstructores = this.instructores.filter((instructor) => {
-  //     const selectedFichaString = this.selectednumeroFicha.toString();
-  //     return (
-  //       !selectedFichaString || instructor.numeroFicha.toString() === selectedFichaString
-  //     );
-  //   });
+    const filtroFichaNumero = Number(this.filtroFicha) || null;
+    console.log('Valor de filtroFichaNumero:', filtroFichaNumero);
+
+    // Filtra la lista según los criterios de filtro
+    usuariosFiltrados = usuariosFiltrados.filter((usuario) => {
+      console.log('Usuario:', usuario);
+      console.log('Número de Ficha del Usuario:', usuario.numeroFicha);
+
+      const cumpleFiltroFicha =
+        filtroFichaNumero === null || (+usuario.numeroFicha === filtroFichaNumero);
+      const cumpleFiltroNombre =
+        !this.filtroNombre || usuario.primerNombre.includes(this.filtroNombre);
+      const cumpleFiltroDocumento =
+        !this.filtroDocumento || usuario.documento === this.filtroDocumento;
+
+      console.log('Cumple Filtro Ficha:', cumpleFiltroFicha);
+      console.log('Cumple Filtro Nombre:', cumpleFiltroNombre);
+      console.log('Cumple Filtro Documento:', cumpleFiltroDocumento);
+
+      return cumpleFiltroFicha && cumpleFiltroNombre && cumpleFiltroDocumento;
+    });
+
+this.filteredInstructores = usuariosFiltrados;
 
 
-  //   console.log('Instructores filtrados:', this.filteredInstructores);
-  // }
+    console.log('Instructores después de aplicar filtros:', this.filteredInstructores);
+  }
 
+
+  limpiarFiltros() {
+    // Restablecer la lista original y los valores de los filtros
+    this.filteredInstructores = this.originalInstructores.slice();
+    this.filtroFicha = null;
+    this.filtroNombre = null;
+    this.filtroDocumento = null;
+  }
 }
+
